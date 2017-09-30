6ccd79475155195c6d03bbd12f4a157e3a2407a8
jianfeng.shen <jianfeng.shen@vipshop.com>
2017-09-30 17:11:24 +0800

    2

--- first2/src/main/java/shenjf/study/first2/App.java
+++ first2/src/main/java/shenjf/study/first2/App.java
@@ -2,12 +2,10 @@ package shenjf.study.first2;
 
 /**
  * Hello world!
- *
+ * VV
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
