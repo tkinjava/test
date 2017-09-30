aada9bae4411cc0637fd08753229d28c172e0983
jianfeng.shen <jianfeng.shen@vipshop.com>
2017-09-30 17:07:51 +0800

    v2

---
+++ first2/src/main/java/shenjf/study/first2/App.java
@@ -0,0 +1,13 @@
+package shenjf.study.first2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
